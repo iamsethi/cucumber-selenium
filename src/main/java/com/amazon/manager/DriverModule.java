217de559a469af12cbce402012f54e28d1eadc0e
ketan.sethi <ketansethi786@gmail.com>
2019-02-13 14:59:18 +0530

    Pass browser name from world

--- src/main/java/com/amazon/manager/DriverModule.java
+++ src/main/java/com/amazon/manager/DriverModule.java
@@ -1,33 +1,18 @@
 package com.amazon.manager;
 
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.interactions.Actions;
-
 import com.google.inject.AbstractModule;
-import com.google.inject.Provides;
 import com.google.inject.Scopes;
+import com.google.inject.name.Names;
 
 public class DriverModule extends AbstractModule {
 
 	@Override
 	protected void configure() {
-		// DriverManager config
-		bind(DriverManager.class).to(ChromeDriverManager.class).in(Scopes.SINGLETON);
+		bind(DriverManager.class).annotatedWith(Names.named("Chrome")).to(ChromeDriverManager.class)
+				.in(Scopes.SINGLETON);
+		bind(DriverManager.class).annotatedWith(Names.named("Firefox")).to(FirefoxDriverManager.class)
+				.in(Scopes.SINGLETON);
 	}
 
-	@Provides
-	public WebDriver getDriver(DriverManager driverManager) {
-		return driverManager.getDriver();
-	}
 
-	@Provides
-	public Actions getActions(WebDriver driver) {
-		return new Actions(driver);
-	}
-
-	@Provides
-	public JavascriptExecutor getExecutor(WebDriver driver) {
-		return (JavascriptExecutor) (driver);
-	}
 }
--- src/main/java/com/amazon/world/World.java
+++ src/main/java/com/amazon/world/World.java
@@ -13,13 +13,18 @@ import com.amazon.manager.DriverManager;
 import com.amazon.manager.DriverModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import com.google.inject.Key;
 import com.google.inject.Singleton;
+import com.google.inject.name.Names;
 
+//Guice-constructor of class will be mapped with configure method  -  IMP
 @Singleton
 public class World {
-
-	Injector driverInjector = Guice.createInjector(new DriverModule()); // Bind Chromedriver to drivermanager
-	public DriverManager driverManager = driverInjector.getInstance(DriverManager.class);
+	// Bind Chromedriver/Firefoxdriver to drivermanager
+	Injector driverInjector = Guice.createInjector(new DriverModule());
+	// Pass bowser name
+	public DriverManager driverManager = driverInjector
+			.getInstance(Key.get(DriverManager.class, Names.named("Firefox")));
 	public WebDriver driver = driverManager.getDriver();
 
 	public JsonDataReader data = new JsonDataReader();
@@ -27,14 +32,14 @@ public class World {
 
 	// Initialize config properties file
 	Injector configInjector = Guice.createInjector(new ConfigModule());
-	// Use above key value in DBConnection
+	// Use above key value in ConfigFileReader class
 	public ConfigFileReader config = configInjector.getInstance(ConfigFileReader.class);
+
 	// Initialize DB properties file
 	Injector dbInjector = Guice.createInjector(new DBModule());
-	// Use above key value in DBConnection
+	// Use above key value in DBConnection class
 	public DBConnection dbService = dbInjector.getInstance(DBConnection.class);
-	// Proxy Pattern + Use above K,V in contructor of
-	// DBUtilProxy
+	// Proxy Pattern + Use above K,V in constructor of DBUtilProxy
 	public DBUtil dbUtil = dbInjector.getInstance(DBUtilProxy.class);
 
 }
